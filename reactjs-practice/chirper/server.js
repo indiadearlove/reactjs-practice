9de9dd387634b5100bff47ba6742b1f13b8b6591
India Dearlove <india_dearlove@yahoo.co.uk>
2016-03-22 14:50:48 +0000

    chirper

---
+++ reactjs-practice/chirper/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+// var login   = require('./login');
+
+express()
+    .set('view engine', 'ejs')
+    .use(express.static('./public'))
+    // .use(login.routes)
+    // .use(require('./chirps'))
+    .get('*', function (req, res) {
+        res.render('index'
+        // 	, {
+        //     user: login.safe(req.user)
+        // }
+        );
+    })
+.listen(3000);
---
+++ reactjs-practice/chirper/src/main.js
@@ -0,0 +1,4 @@
+var events = require('events');
+
+console.log('hello');
+console.log(events);
